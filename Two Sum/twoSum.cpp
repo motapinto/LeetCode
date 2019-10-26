3f16c1da77044bd0c5af32d92392f8a5f1a39b00
Martim de Carvalho e Sousa Pinto da Silva <41308685+motapinto@users.noreply.github.com>
2019-10-26 16:31:19 +0100

    Update twoSum.cpp

--- Two Sum/twoSum.cpp
+++ Two Sum/twoSum.cpp
@@ -24,3 +24,31 @@ public:
         return result;
     }
 };
+//Runtime: 8 ms, faster than 92.80% of C++ online submissions for Two Sum.
+//Solution for speed
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int> &numbers, int target) {
+        ////unordered_map<number(key), index(value)>
+        vector<int> result;
+        
+        //If we fix a number X
+        //then we just need Y = target - X
+        
+        for (int i = 0; i < numbers.size(); i++) {
+            int complement = target - numbers[i];
+            auto it = find(numbers.begin(), numbers.end(), complement);
+            if(it != numbers.end() && distance(numbers.begin(), it) != i) {
+                //numbers.clear();
+                result.push_back(distance(numbers.begin(), it));
+                result.push_back(i);
+                return result;
+            }
+        }
+        return result;
+    }
+};
+
+//Memory Usage: 9.2 MB, less than 95.37% of C++ online submissions for Two Sum.
+//Solution for memory
